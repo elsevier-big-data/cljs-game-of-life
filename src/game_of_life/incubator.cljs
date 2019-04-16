9947ff58534557203621e7b73c6d7562bfb6500c
Callum Turnbull <callum@turnbull.codes>
2019-04-16 17:43:20 +0100

    Added game of life logic and speed buttons

--- src/game_of_life/incubator.cljs
+++ src/game_of_life/incubator.cljs
@@ -1,34 +1,63 @@
 (ns game-of-life.incubator
   (:require [game-of-life.state :refer [state update-state!]]
-            [reagent.core :as reagent]))
+            [reagent.core :as reagent]
+            [antizer.reagent :as ant]))
 
 (def width 50)
 (def height 50)
 (def block-size 10)
 
-(defn random-grid [grid]
-  (let [random-coords (repeatedly #(vec [(rand-int width) (rand-int height)]))]
-    (into #{} (take (rand-int (* width height)) random-coords))))
+;; Logic
+;; --------------------------------------------------
+(defn get-neighbours
+  [[x y]]
+  [[(dec x) (dec y)] [x (dec y)] [(inc x) (dec y)]
+   [(dec x) y] [(inc x) y]
+   [(dec x) (inc y)] [x (inc y)] [(inc x) (inc y)]])
+
+
+(defn is-alive?
+  [grid coord]
+  (let [pop-count (count (select-keys grid (get-neighbours coord)))
+        is-alive (contains? grid coord)]
+    (cond
+      (and is-alive (< pop-count 2)) false
+      (and is-alive (<= 2 pop-count 3)) true
+      (and is-alive (> pop-count 3)) false
+      (and (not is-alive) (= pop-count 3)) true
+      :else false)))
+
+(defn logic
+  [grid]
+  (into {} (for [x (range 0 width)
+                 y (range 0 height)
+                 :when (is-alive? grid [x y])]
+             [[x y] :live]))
+  )
 
 (defn tick []
   (update-state! #(-> %
                       (update-in [:time] inc)
-                      (update-in [:grid] random-grid))))
+                      (update-in [:grid] logic))))
 
-(defn- recurring-timer [f period]
-  (letfn [(trigger-and-renew [] (do
-                                  (f)
-                                  (js/setTimeout trigger-and-renew period)))]
-    (js/setTimeout trigger-and-renew period)))
+(defn- recurring-timer [f]
+
+  (letfn [(trigger-and-renew []
+            (let [period (:delay @state)]
+              (do
+                (f)
+                (js/setTimeout trigger-and-renew period))))]
+    (js/setTimeout trigger-and-renew 0)))
 
 (defonce tick-timer
-  (recurring-timer tick 500))
+         (recurring-timer tick))
+
 
 ;; View
 ;; --------------------------------------------------
 
 (def background-color "#0D0208")
-(def block-color      "#008F41")
+(def block-color "#008F41")
 
 (defn- draw-cell [ctx [x y]]
   (.fillRect ctx (* x block-size) (* y block-size) block-size block-size))
@@ -38,30 +67,45 @@
     (set! (.-fillStyle ctx) background-color)
     (.fillRect ctx 0 0 (* width block-size) (* height block-size))
     (set! (.-fillStyle ctx) block-color)
-    (dorun (map (partial draw-cell ctx) (:grid @state)))))
+    (dorun (map (partial draw-cell ctx) (keys (:grid @state))))))
 
 (defn- div-with-canvas
   "A canvas within a :div, which will render (f-draw context)."
   [properties f-draw]
   (let [dom-node (reagent/atom nil)]
     (reagent/create-class
-     {:display-name "div-with-canvas"
+      {:display-name "div-with-canvas"
 
-      :component-did-mount
-      (fn [this]
-        (reset! dom-node (reagent/dom-node this)))
+       :component-did-mount
+                     (fn [this]
+                       (reset! dom-node (reagent/dom-node this)))
 
-      ;; Arguments must match the outer arguments. Also, because reagent-render is called prior to mounting, we need to
-      ;; protect against the null dom-node on the first render
-      :reagent-render
-      (fn [properties f-draw]
-        (if-let [node @dom-node] (f-draw (.-firstChild node)))
-        [:div [:canvas properties]])})))
+       ;; Arguments must match the outer arguments. Also, because reagent-render is called prior to mounting, we need to
+       ;; protect against the null dom-node on the first render
+       :reagent-render
+                     (fn [properties f-draw]
+                       (if-let [node @dom-node] (f-draw (.-firstChild node)))
+                       [:div [:canvas properties]])})))
+
+(defn- increment-delay
+  []
+  (update-state! #(-> %
+                      (update-in [:delay] (partial + 100)))))
+(defn- decrement-delay
+  []
+  (update-state! #(-> %
+                      (update-in [:delay] (fn [x] (- x 100))))))
 
 (defn view [params]
-  [:div {:style {:padding "5px"}}
+  [:div {:style {:padding "5px"}}Ò
    [:h1 "Game of Life"]
    [:div {:style {:padding "5px"}}
     [:p (str "Generation: " (:time @state))]
+    [:p (str "Delay: " (:delay @state))]
+    [ant/form {:layout "inline"}
+     [ant/form-item {}
+      [ant/button {:type "primary" :on-click increment-delay} "Slower"]]
+     [ant/form-item {}
+      [ant/button {:type "primary" :on-click decrement-delay} "Faster"]]]
     [div-with-canvas {:width  (* width block-size)
                       :height (* height block-size)} draw-grid]]])
--- src/game_of_life/state.cljs
+++ src/game_of_life/state.cljs
@@ -2,9 +2,19 @@
   (:require
    [reagent.core :as reagent]))
 
+
+(def width 50)
+(def height 50)
+
+(defn random-grid []
+  (let [random-coords (repeatedly #(vec [(rand-int width) (rand-int height)]))]
+    (into {} (for [k (take (rand-int (* width height)) random-coords)]
+       [k :live]))))
+
 ;; Application state
 (defonce state (reagent/atom {:time 0
-                              :grid #{}}))
+                              :grid (random-grid)
+                              :delay 500}))
 
 (defn update-state! [f & args]
   (swap! state f)
